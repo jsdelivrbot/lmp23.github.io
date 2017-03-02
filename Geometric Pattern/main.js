93ac3b39600139f76d5316794fe892f9fe5a7d70
Leah Patzkowsky <lmfreesky241@gmail.com>
2017-03-02 21:05:05 +0000

    Add geometric_pattern project

---
+++ Geometric Pattern/main.js
@@ -0,0 +1,15 @@
+var NUM_CIRCLES = 12;
+var circleDiameter;
+
+function setup() {
+    createCanvas(480, 600);
+    circleDiameter = width/NUM_CIRCLES;
+}
+
+function draw() {
+    var x=0;
+    while (x <= width) {
+      ellipse(x, 0, circleDiameter, circleDiameter);
+        x = x + circleDiameter;
+    }
+}
